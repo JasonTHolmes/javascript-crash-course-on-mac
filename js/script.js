ebd8c2e7bd91befd4f89978551f923f97de1f3e9
Jason Holmes <JasonThomasHolmes@Gmail.com>
2021-04-03 14:12:36 -0700

    First Comment

---
+++ js/script.js
@@ -0,0 +1,24 @@
+console.log('hello');
+
+// alert('hello this is jason');
+
+// how to write a comment inline
+
+// variables
+var b = 'smoothie';
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+// Manipulate DOM with JavaScript
+/* ... Just a fancy way of saying 
+change HTML with JavaScript. */
+
+// var age = prompt('What is your age?');
+
+// document.getElementById('someText').innerHTML = age;
+
+// Numbers in JavaScript
+var num = 5.7;
+console.log(5 / 10);
